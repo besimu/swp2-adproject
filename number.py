c0c67ce38745be21715cb5c96c630015dc381764
besimu <76421201+besimu@users.noreply.github.com>
2021-12-07 16:41:00 +0900

    Update number.py

--- number.py
+++ number.py
@@ -1,1 +1,1 @@
-number = ['1.png', '2.png', '3.png', '4.png', '5.png','6.png','7.png','8.png','9.png']
+number = ['0.png','1.png', '2.png', '3.png', '4.png', '5.png','6.png','7.png','8.png','9.png']
